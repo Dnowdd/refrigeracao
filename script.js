911b50d3eac907358e38405cac4cb5c9559021f5
David Aquino (PC) <dqueiroz2005@gmail.com>
2023-04-10 17:49:22 -0300

    Melhorando a responsividade

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ function changeStyleHeader(){
         $("#header").removeClass("sticky");
     }
 }
+
+
+
+
+
+$(document).ready(function(){
+    $('.carousel').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+      draggable: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
+  
